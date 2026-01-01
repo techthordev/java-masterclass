cb74971deb6abeb9a85d76d160407645a89ba65c
techthordev <thorsten.fey@outlook.com>
2025-12-31 21:12:20 -0300

    initial upload

---
+++ src/br/com/techthordev/_02_keywords_and_expressions/Main.java
@@ -0,0 +1,20 @@
+package br.com.techthordev._02_keywords_and_expressions;
+
+public class Main {
+
+    void main() {
+
+        int myVariable = 50;
+
+        myVariable++;
+        myVariable--;
+
+        System.out.println("This is a test");
+
+        System.out.println("This is" +
+                " another" +
+                " still more.");
+
+        int anothervariable=50;myVariable--;System.out.println("my variable: " + myVariable);
+    }
+}
